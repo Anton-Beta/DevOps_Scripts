10da9ae822d22148e313b19f6f3185a1f637e368
Anton <h44iprasath@proton.me>
2025-11-28 15:16:00 +0530

    Inputs/Redirecting

---
+++ Basics/Inputs/pilot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this is the name of the file itself $0
+echo $0 
+
+echo $3 $1 $2
+
+echo $?
---
+++ Basics/STD-in-opt/redirect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+$1 1>op.txt 2>error.txt
+
+#op.txt is the output file 
+#error.txt is the file theat shows if any error found 
